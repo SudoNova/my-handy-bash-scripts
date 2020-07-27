aa1f06cd708a8ac38455523540f2c5429ed48ed4
SuNova <sunova@tutanota.com>
2020-07-28 02:57:53 +0430

    Init

---
+++ my-aliases.sh
@@ -0,0 +1,25 @@
+set -a # Export everything you see here
+
+# I'm lazy piping to less :)
+# In some cases like sudo this doesn't work because of shell-subshell problem.
+toless ()
+{
+	args=( $@ )
+	$1 ${args[@]:1} | less
+}
+
+# This is a python module that compiles other python modules in cpython style.
+alias pycompile="python -m compileall"
+
+# This sometimes returns no result.
+ipresolve()
+{
+	dig +noall +answer $1 | cut -f 6
+
+}
+
+# Installing such packages with pip and using pycompile is much more convenient.
+# However, I'm not sure about CFLAGS, LDFLAGS and other compile related flags in this case.
+alias tldr="python -m tldr" 
+
+set +a # Stop exporting
