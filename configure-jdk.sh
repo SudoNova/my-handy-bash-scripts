3d3c7e1ec65ee68cf00fab0c2d9e28f47b9c824a
Alireza Mohamadi <sunova@tutanota.com>
2020-12-14 01:22:59 +0330

    Print logs

--- configure-jdk.sh
+++ configure-jdk.sh
@@ -2,4 +2,4 @@
 
 ## Assuming we are in base directory
 
-bash configure --with-extra-cflags="$CFLAGS" --with-extra-cxxflags="$CXXFLAGS" --with-extra-ldflags="$LDFLAGS" --disable-warnings-as-errors
+bash configure --with-extra-cflags="$CFLAGS" --with-extra-cxxflags="$CXXFLAGS" --with-extra-ldflags="$LDFLAGS" --disable-warnings-as-errors --with-log=info
